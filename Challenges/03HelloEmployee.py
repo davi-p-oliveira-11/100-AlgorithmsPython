cf94db2b1e84c04b2f9fa40f6bb71199875368e2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-17 12:57:16 -0300

    updated algortithm 03

---
+++ Challenges/03HelloEmployee.py
@@ -0,0 +1,15 @@
+'''
+3) Create a program that reads an employee's name and salary, and displays a message at the end.  
+Example:  
+Employee's Name: Maria do Carmo  
+Salary: 1850.45  
+The employee Maria do Carmo has a salary of R$1850.45 in June.
+'''
+
+name = input("What is the employee name ? ")
+salary = input("How much is the employee salary ? ")
+month = input("Last month worked ? ")
+
+print(f'The employee {name} has a salary of {salary} in {month}')
+
+input("Press enter to exit ... ")
