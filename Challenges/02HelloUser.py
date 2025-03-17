49026d16a64f9c5d1c14ee7831dec7ed47096753
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-17 12:54:06 -0300

    updated algorithm 02

---
+++ Challenges/02HelloUser.py
@@ -0,0 +1,11 @@
+'''
+2) Write a program that reads a person's name and displays a welcome message for them:  
+Example:  
+What is your name? João da Silva  
+Hello João da Silva, it's a pleasure to meet you!
+'''
+
+name = input("What is your name ? ")
+print(f'Hello {name} nice to meet you !')
+
+input('Press Enter to Exit ...')
