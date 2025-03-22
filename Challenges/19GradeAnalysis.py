e328fbf98e5f3f2fa56c4779bf963e1e4d4a2c7c
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-22 20:12:57 -0300

    updated algorithm 19

---
+++ Challenges/19GradeAnalysis.py
@@ -0,0 +1,6 @@
+'''
+19) Create an algorithm that reads a student's name and two grades, 
+calculates their average, and displays it on the screen. 
+In the end, analyze the average and show whether 
+the student had good performance (if it was above the average of 7.0).
+'''
