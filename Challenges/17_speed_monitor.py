b347b5c60a2ae772998f2a51f4523376cb1f65b6
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-23 16:56:42 -0300

    updated algorithm 17

--- Challenges/17_speed_monitor.py
+++ Challenges/17_speed_monitor.py
@@ -4,3 +4,12 @@ If it exceeds 80 km/h, display a message saying that the user has been fined.
 In this case, display the amount of the fine, charging R$5 for each km above the allowed speed.
 '''
 
+car_speed = int(input("What speed did the car reach  ? "))
+
+if (car_speed > 80):
+  print(f'''The car reached a speed of {car_speed}, exceeding {car_speed - 80} km/h of the allowed speed, 
+         The driver wiil have to pay a fine of {(car_speed - 80) * 5} USD)''')
+else :
+  print("The driver did not exceed the speed limit, so they will not have to pay a fine.")  
+
+input("Press Enter to Exit ... ")
