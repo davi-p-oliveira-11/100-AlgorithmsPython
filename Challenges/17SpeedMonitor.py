af49efea6c285ee1e2d0b6186a7f7c5e90bbea70
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-23 15:11:16 -0300

    updated algorithm 12 to snake case

--- Challenges/17SpeedMonitor.py
+++ Challenges/17SpeedMonitor.py
@@ -3,3 +3,4 @@
 If it exceeds 80 km/h, display a message saying that the user has been fined. 
 In this case, display the amount of the fine, charging R$5 for each km above the allowed speed.
 '''
+
