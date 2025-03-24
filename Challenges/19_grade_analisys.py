96f7c44b71c81146619d829021459bec41e4e2ce
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 15:01:25 -0300

    updated algorithm 19 logic

--- Challenges/19_grade_analisys.py
+++ Challenges/19_grade_analisys.py
@@ -4,3 +4,14 @@ calculates their average, and displays it on the screen.
 In the end, analyze the average and show whether 
 the student had good performance (if it was above the average of 7.0).
 '''
+
+grade_1 = float(input("Enter first exam grade: "))
+grade_2 = float(input("Enter second exam grade: "))
+average = (grade_1 + grade_2) / 2
+
+print(f"The student average is: {average:.1f}")
+
+if (average >= 7):
+  print("Student Passed !")
+else:
+  print("Student Failed !")
