2680fab3b1d7c516388db04bc2cb3c4d14c7b1ca
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 15:08:17 -0300

    updated algorithm 21

--- Challenges/20_even_or_odd.py
+++ Challenges/20_even_or_odd.py
@@ -1,3 +1,9 @@
 '''
 20) Develop a program that reads an integer and shows whether it is EVEN or ODD."
 '''
+
+num = int(input("Enter a number: " ))
+
+print("Even Number" if num % 2 == 0 else "Odd Number")
+
+input("Press Enter to Exit ... ")
