84d16c0818831496861585435c6874af78abe0ac
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 15:16:31 -0300

    updated algorithm 22 statement

---
+++ Challenges/22_enlistment_check.py
@@ -0,0 +1,7 @@
+'''
+22) Write a program that reads a guy's birth year and displays his situation 
+regarding military enlistment.
+
+If he is under 18 years old, show how many years are left until enlistment.
+If he is over 18 years old, show how many years have passed since enlistment.
+'''
