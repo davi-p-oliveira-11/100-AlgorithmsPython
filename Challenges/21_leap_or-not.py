cf58f26b362e094254d0cabde536d8243c4d8130
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 15:14:24 -0300

    updated algorithm 21

--- Challenges/21_leap_or-not.py
+++ Challenges/21_leap_or-not.py
@@ -1,3 +1,12 @@
 '''
 21) Write an algorithm that reads a specific year and shows whether it is a LEAP YEAR or not."
 '''
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+  print(f"{year} is leap.")
+else:
+  print(f"{year} is not leap")
+
+input("Press Enter to Exit ... ")
