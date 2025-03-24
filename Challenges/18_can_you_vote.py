471d138c380084c56dbf0a3e2546c2eb273d7a27
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 14:53:18 -0300

    updated algorithm 18 logic

--- Challenges/18_can_you_vote.py
+++ Challenges/18_can_you_vote.py
@@ -2,3 +2,17 @@
 18) Write a program that reads a person's year of birth, 
 calculates their age, and then shows whether they are eligible to vote or not.
 '''
+
+from datetime import datetime
+
+current_year = datetime.now().year
+year_of_birth = int(input("What year were you born ? "))
+
+age = current_year - year_of_birth;
+
+if (age >= 16):
+  print("You are already eligible to vote.")
+else:
+  print("You are not old enough to vote yet.")
+
+input("Press Enter to Exit ... ")
