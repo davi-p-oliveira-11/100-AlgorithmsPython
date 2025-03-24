5ec933af2de2916c995813885998d5b81deb1e8e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 15:25:37 -0300

    updated algorithm 22 logic

--- Challenges/22_enlistment_check.py
+++ Challenges/22_enlistment_check.py
@@ -5,3 +5,23 @@ regarding military enlistment.
 If he is under 18 years old, show how many years are left until enlistment.
 If he is over 18 years old, show how many years have passed since enlistment.
 '''
+
+
+from datetime import datetime
+
+year_of_birth = int(input("What year were you born ? "))
+current_year = datetime.now().year
+user_age = current_year - year_of_birth
+years_remaining = max(0, 18 - user_age)
+year_that_complete_18 = year_of_birth + 18
+years_passed = current_year - year_that_complete_18
+
+if (user_age < 18):
+  print(f"You're not old enough to enlist yet. You'll have to wait {years_remaining} more years.")
+elif (user_age == 18):
+  print("You are the right age to enlist this year.")
+else:
+  print(f"You missed your enlistment by {years_passed}")
+
+
+input("Press Enter to Exit ... ")
