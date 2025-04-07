f7d28468c869f2b67a7578ff487ce3a0de694c17
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 18:35:28 -0300

    added logic with pep formatting

--- Challenges/26_compare_numbers.py
+++ Challenges/26_compare_numbers.py
@@ -1,7 +1,19 @@
-'''
+"""
 26) Write an algorithm that reads two integers and compares them, displaying
-      one of the following messages on the screen:
+one of the following messages on the screen:
 - The first value is greater
 - The second value is greater
 - There is no greater value, both are equal
-'''
+"""
+
+num_1 = int(input("Type a number: "))
+num_2 = int(input("Type another number: "))
+
+if num_1 > num_2:
+    print("The first value is greater.")
+elif num_1 < num_2:
+    print("The second value is greater.")
+else:
+    print("There is no greater value, both are equal.")
+
+input("Press Enter to Exit ... ")
