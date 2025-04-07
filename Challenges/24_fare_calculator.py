be6ca20f2242cd42e4991b3d8cf9a704a4112815
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 16:23:42 -0300

    updated algorithm 24 logic

--- Challenges/24_fare_calculator.py
+++ Challenges/24_fare_calculator.py
@@ -3,3 +3,13 @@
       to travel in Km. Calculate the ticket price, charging $0.50 per Km for
       trips up to 200Km and $0.45 for longer trips.
 '''
+
+distance_traveled = int(input(" How many kilometers will you travel ? "))
+
+if distance_traveled <= 200:
+  print(f"You will pay {distance_traveled * 0.5} USD")
+else:
+  print(f"You will pay {distance_traveled * 0.45} USD")
+
+
+input("Press Enter to Exit ... ")
