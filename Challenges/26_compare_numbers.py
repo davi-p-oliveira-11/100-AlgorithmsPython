a24cc4c8d08fc63cc85bb50ddd4e5e66301beec0
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 18:29:54 -0300

    updated algorithm 26 statement

---
+++ Challenges/26_compare_numbers.py
@@ -0,0 +1,7 @@
+'''
+26) Write an algorithm that reads two integers and compares them, displaying
+      one of the following messages on the screen:
+- The first value is greater
+- The second value is greater
+- There is no greater value, both are equal
+'''
