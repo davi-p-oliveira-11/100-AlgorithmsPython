fec2cd0cb9bc9fa23cbaae109747afc2796efdbe
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 16:04:19 -0300

    updated algorithm 23 logic

--- Challenges/23_MenWomenDiscount.py
+++ Challenges/23_MenWomenDiscount.py
@@ -10,7 +10,27 @@ Women get a 13% discount
 
 '''
 
-customer_name = input("Type your namae: ")
+customer_name = input("Type your name: ")
 customer_gender = input("Enter your gender F for female and M male:").upper()
+purchase_amount = float(input("What was the total value of the purchase ? "))
 
-print(customer_gender)
+men_discount = purchase_amount * 0.05
+women_discount = purchase_amount * 0.13
+
+complete_value_men = purchase_amount - men_discount
+complete_value_women = purchase_amount - women_discount
+
+if customer_gender == 'F':
+  print(f'''{customer_name} made a purchase of {purchase_amount:.2f} USD
+             and earned a discount of 13% and will pay {complete_value_women} USD
+        ''')
+elif customer_gender == 'M':
+  print(f'''
+            {customer_name} made a purchase of {purchase_amount:.2f} USD
+            and earned a discount of 5% and will pay {complete_value_men} USD
+        ''')
+else:
+  print("Enter a valid gender, F for female M for male !")
+
+
+input("Press Enter to Exit ... ")
