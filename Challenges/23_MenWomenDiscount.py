10b8a131504e79be8f025eaf5dce5a7657f3d6a2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 15:29:55 -0300

    updated algorithm 23 statement

---
+++ Challenges/23_MenWomenDiscount.py
@@ -0,0 +1,16 @@
+'''
+23) In an exclusive promotion for Women's Day, 
+a store wants to give discounts to everyone, 
+but especially to women. 
+Create a program that reads the customer's name, gender, 
+and the purchase value and calculates the price with a discount. Knowing that:   
+
+Men get a 5% discount
+Women get a 13% discount 
+
+'''
+
+customer_name = input("Type your namae: ")
+customer_gender = input("Enter your gender F for female and M male:").upper()
+
+print(customer_gender)
