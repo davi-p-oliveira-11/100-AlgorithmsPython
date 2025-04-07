79f90c831f1adc2b797ceb65ef022db3dd52903d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-07 16:06:45 -0300

    updated algorithm 24 logic

---
+++ Challenges/24_fare_calculator.py
@@ -0,0 +1,5 @@
+'''
+  24) Write an algorithm that asks for the distance a passenger wants
+      to travel in Km. Calculate the ticket price, charging $0.50 per Km for
+      trips up to 200Km and $0.45 for longer trips.
+'''
