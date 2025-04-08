5c4f27049499ce313c356bf94c5d033d69ec6ae5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-08 18:50:01 -0300

    updated algorithm 28 statement

---
+++ Challenges/28_land_classification.py
@@ -0,0 +1,11 @@
+"""
+ 28) Write a program that asks the user to input the width and length of a 
+rectangular plot of land. The program should then calculate and display the 
+area of the land in square meters. 
+Additionally, the program should classify the
+land based on the following criteria:
+
+Less than 100 square meters: POPULAR
+Between 100 and 500 square meters: MASTER
+Greater than 500 square meters: VIP
+"""
