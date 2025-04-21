03c0959478b68d9f1f5049c14d12ccb4bc038ce8
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:39:35 -0300

    added algorithm 39

---
+++ Challenges/39_count_10_to_3.py
@@ -0,0 +1,15 @@
+'''
+ 39) Create an algorithm that displays the following count on the screen:
+10 9 8 7 6 5 4 3 Done!
+'''
+
+counter = 10
+
+while counter >= 3:
+  print(counter)
+  counter -= 1
+
+
+print("It's over ! ")
+
+input("Press Enter to exit ..." )
