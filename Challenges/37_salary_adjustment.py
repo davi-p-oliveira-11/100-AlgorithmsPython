3bf2909b77d91988aaba8f5fb05d5e8c0c3487f3
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:26:29 -0300

    updated algorithm 37 logic

--- Challenges/37_salary_adjustment.py
+++ Challenges/37_salary_adjustment.py
@@ -14,3 +14,41 @@ From 20 to 30 years at the company: +13%
 More than 30 years at the company: +25%
 
 '''
+
+# Salary Adjustment Program
+
+current_salary = float(input("Enter the employee's current salary: "))
+
+gender = input("Enter the employee's gender (M for Male, F for Female): ").upper()
+
+years_worked = int(input("Enter the number of years the employee has worked at the company: "))
+
+new_salary = current_salary
+percentage_increase = 0
+
+if gender == 'F':
+    if years_worked < 15:
+        percentage_increase = 5
+    elif years_worked <= 20:
+        percentage_increase = 12
+    else:
+        percentage_increase = 23
+elif gender == 'M':
+    if years_worked < 20:
+        percentage_increase = 3
+    elif years_worked <= 30:
+        percentage_increase = 13
+    else:
+        percentage_increase = 25
+else:
+    print("Error: Please enter a valid gender (M or F).")
+    exit()
+
+new_salary += (new_salary * percentage_increase) / 100
+
+print(f"The employee's previous salary was R${current_salary:.2f}")
+print(f"With an increase of {percentage_increase}%, the new salary is R${new_salary:.2f}")
+
+
+
+input("Press Enter to exit ... ")
