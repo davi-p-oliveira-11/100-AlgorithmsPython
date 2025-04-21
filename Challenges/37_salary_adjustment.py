cd2269ca997b79a5b57c56b9cf938424f90403b2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:23:59 -0300

    updated statement

---
+++ Challenges/37_salary_adjustment.py
@@ -0,0 +1,16 @@
+'''
+  37) A company needs to adjust its employees' salaries by giving a raise based on certain factors. 
+Write a program that reads the current salary, the employee's gender, 
+and how many years they have worked at the company. 
+In the end, display their new salary based on the following table: Women
+
+Less than 15 years at the company: +5%
+From 15 to 20 years at the company: +12%
+More than 20 years at the company: +23%
+Men
+
+Less than 20 years at the company: +3%
+From 20 to 30 years at the company: +13%
+More than 30 years at the company: +25%
+
+'''
