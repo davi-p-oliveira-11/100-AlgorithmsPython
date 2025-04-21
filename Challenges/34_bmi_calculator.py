d8694e441345d807aeef7b4072123037e49f21d5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:01:43 -0300

    added stataement

---
+++ Challenges/34_bmi_calculator.py
@@ -0,0 +1,12 @@
+'''
+ 34) The Body Mass Index (BMI) is a value calculated based on a person's height and weight. 
+According to the BMI value, we can classify the individual within certain ranges.   
+
+below 18.5: Underweight
+between 18.5 and 25: Normal weight   
+between 25 and 30: Overweight
+between 30 and 40: Obese
+above 40: Morbidly obese 
+
+Note: BMI is calculated by the expression weight/height² (weight divided by height squared).
+'''
