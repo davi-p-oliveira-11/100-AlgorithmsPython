6dfa0691743f76d17491b0440b0e4e9019d38c44
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:54:31 -0300

    updated algorithm 40

---
+++ Challenges/40_count_0_to_18.py
@@ -0,0 +1,15 @@
+'''
+ 40) Write a program that writes on the screen the following count:
+0 3 6 9 12 15 18 It's over !
+'''
+
+counter = 0
+
+while counter <= 18:
+  print(counter)
+  counter += 3
+
+
+print("It's over ! ")
+
+input("Press Enter to exit ... ")
