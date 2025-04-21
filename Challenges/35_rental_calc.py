a053bda56d5fc6f3a8cc05323e54af68c676a420
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:05:35 -0300

    updated algorithm 35 statement

---
+++ Challenges/35_rental_calc.py
@@ -0,0 +1,15 @@
+'''
+ 35) A car rental company needs to charge for its services. 
+Renting a popular car costs R$90 per day, and a luxury car costs R$150 per day. 
+Additionally, the customer pays per kilometer driven. 
+Create a program that reads the type of car rented (popular or luxury), 
+the number of rental days, and the number of kilometers driven. 
+At the end, display the price to be paid according to the following table:
+
+Popular cars (90USD rental per day)
+Up to 100km driven: 0.20USD per km
+Over 100km driven: 0.10USD per km
+Luxury cars (150USD rental per day)
+Up to 100km driven: 0.30USD per km
+Over 100km driven: 0.25USD per km
+'''
