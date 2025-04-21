1ad1eca3f23b0b58bc028043927bd9b7023f5a7e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:07:41 -0300

    added algorithm 35 logic

--- Challenges/35_rental_calc.py
+++ Challenges/35_rental_calc.py
@@ -13,3 +13,42 @@ Luxury cars (150USD rental per day)
 Up to 100km driven: 0.30USD per km
 Over 100km driven: 0.25USD per km
 '''
+
+
+rented_car = input(
+    'Would you like to rent a Popular or Luxury car? Enter P for Popular and L for Luxury: '
+).upper()
+
+days_rented = int(input('For how many days did you use the car? '))
+distance_traveled = float(input('Enter how many kilometers were driven with the car: '))
+
+price_for_days_rented_popular = days_rented * 90
+price_for_days_rented_luxury = days_rented * 150
+
+popular_fare1 = distance_traveled * 0.2
+popular_fare2 = distance_traveled * 0.1
+luxury_fare1 = distance_traveled * 0.3
+luxury_fare2 = distance_traveled * 0.25
+
+if rented_car != 'P' and rented_car != 'L':
+    print('Error: Please enter a valid car category.')
+elif rented_car == 'P' and distance_traveled >= 100:
+    print(f'You rented a Popular car for {days_rented} days.')
+    print(f'Traveled {distance_traveled} km with the car.')
+    print(f'You will pay a total of {price_for_days_rented_popular + popular_fare1:.2f} USD.')
+elif rented_car == 'P' and distance_traveled < 100:
+    print(f'You rented a Popular car for {days_rented} days.')
+    print(f'Traveled {distance_traveled} km with the car.')
+    print(f'You will pay a total of {price_for_days_rented_popular + popular_fare2:.2f} USD.')
+elif rented_car == 'L' and distance_traveled >= 100:
+    print(f'You rented a Luxury car for {days_rented} days.')
+    print(f'Traveled {distance_traveled} km with the car.')
+    print(f'You will pay a total of {price_for_days_rented_luxury + luxury_fare1:.2f} USD.')
+elif rented_car == 'L' and distance_traveled < 100:
+    print(f'You rented a Luxury car for {days_rented} days.')
+    print(f'Traveled {distance_traveled} km with the car.')
+    print(f'You will pay a total of {price_for_days_rented_luxury + luxury_fare2:.2f} USD.')
+
+
+
+input("Press Enter to exit ... ")
