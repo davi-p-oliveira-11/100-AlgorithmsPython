5c565fa5d136b39f9e600a8ea8c1c527780cdd86
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-21 18:34:32 -0300

    updated algorithm 38

---
+++ Challenges/38_count_6_to_11.py
@@ -0,0 +1,14 @@
+'''
+ 38) Write a program that displays the following count on the screen: 6 7 8 9 10 11 Done!
+'''
+
+counter = 6
+
+while counter <= 11:
+  print(counter)
+  counter += 1
+
+
+print("It's over ! ")
+
+input("Press Enter to exit !" )
