ca3f51aff292ea5c0aadc780939b8cad89a3f421
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 18:12:59 -0300

    added algorithm 50 statement

---
+++ Challenges/50_draw_numbers.py
@@ -0,0 +1,6 @@
+'''
+  50) Develop a program that draws 20 numbers between 0 and 10 and displays on the screen:
+a) Which numbers were drawn
+b) How many numbers are greater than 5
+c) How many numbers are divisible by 3
+'''
