52d610f569a92e4215876f6694320acc36dc465f
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 17:19:04 -0300

    updated algorithm 47 logic

--- Challenges/47_sum_of_500.py
+++ Challenges/47_sum_of_500.py
@@ -2,3 +2,15 @@
   47) Develop an application that displays the result of the 
       expression 500 + 450 + 400 + 350 + 300 + ... + 50 + 0
 '''
+
+sum_3 = 0
+counter = 500
+
+while counter != 0:
+  sum_3 += counter
+  counter -= 50
+
+
+print(f"The sum between 500 + 450 + 400 + 350 + 300 ... + 50 + 0 is: {sum_3}")
+
+input("Press Enter to exit ... ")
