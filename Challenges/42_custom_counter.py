2aabf9d3058179dc0a1d57b97f1578692f8e7e8f
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 13:53:16 -0300

    updated algorithm 42

---
+++ Challenges/42_custom_counter.py
@@ -0,0 +1,6 @@
+'''
+42) Write an algorithm that asks the user for any positive integer 
+and displays a count up to that value:
+Example: Enter a value: 35
+Count: 1 2 3 4 5 6 7 ... 33 34 35 Done!
+'''
