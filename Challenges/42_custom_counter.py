601b1d2f660f51b0b8d7200d64e8dd626c684360
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 13:57:07 -0300

    updated algorith 42 logic

--- Challenges/42_custom_counter.py
+++ Challenges/42_custom_counter.py
@@ -4,3 +4,15 @@ and displays a count up to that value:
 Example: Enter a value: 35
 Count: 1 2 3 4 5 6 7 ... 33 34 35 Done!
 '''
+
+user_number = int(input("Enter a positive a value: "))
+counter = 1
+
+while (counter <= user_number):
+  print(counter)
+  counter += 1
+
+
+print("It's over ! ")
+
+input("Press Enter to Exit ... ")
