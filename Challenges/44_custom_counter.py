701c50e487c456ce07606c6aa135a3f7b61e6599
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 14:24:13 -0300

    updated algorithm 44

---
+++ Challenges/44_custom_counter.py
@@ -0,0 +1,8 @@
+'''
+44) Create an algorithm that reads the initial value of the count, the final value, and the increment, then displays all the values within the interval:
+Example:
+Enter the first value: 3
+Enter the last value: 10
+Enter the increment: 2
+Counting: 3 5 7 9 It's over!
+'''
