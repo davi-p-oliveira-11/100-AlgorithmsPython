e9d8469667943dcd056f02ca1b928e85082beab9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 17:40:19 -0300

    added algorithm 49

--- Challenges/49_count_even_odd_numbers.py
+++ Challenges/49_count_even_odd_numbers.py
@@ -3,3 +3,23 @@
        shows how many of them are even and how many are odd.
 '''
 
+counter = 0
+even_numbers = 0
+odd_numbers = 0
+
+while counter < 6:
+  number = int(input(f"Enter an integer {counter + 1}: "))
+
+  if number % 2 == 0:
+    even_numbers += 1
+  else:
+    odd_numbers += 1
+
+  counter += 1 
+
+
+print(f"You entered {even_numbers} even numbers`")
+print(f"You entered {odd_numbers} odd numbers`")
+
+
+input("Press Enter to Exit ... ")
