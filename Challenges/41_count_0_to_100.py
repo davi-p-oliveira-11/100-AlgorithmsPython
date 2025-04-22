e696521ad751515ad6c4285a1e58ec113b63a159
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 13:41:51 -0300

    updated algorithm 41

--- Challenges/41_count_0_to_100.py
+++ Challenges/41_count_0_to_100.py
@@ -2,3 +2,13 @@
  41) Develop a program that shows the following count:
      100 95 90 85 80 ... 0 It's over !
 '''
+
+counter = 100
+
+while (counter >= 0):
+  print(counter)
+  counter -= 5
+
+print("It's over !")
+
+input("Press Enter to Exit ... ")
