0a6c236e4fafa6a641661011b0a930719dc4e108
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 18:16:00 -0300

    added algorithm 50 logic

--- Challenges/50_draw_numbers.py
+++ Challenges/50_draw_numbers.py
@@ -4,3 +4,26 @@ a) Which numbers were drawn
 b) How many numbers are greater than 5
 c) How many numbers are divisible by 3
 '''
+
+import random
+
+numbers = []
+counter = 0
+greater_than_5 = 0
+divisible_by_3 = 0
+
+while counter < 20:
+    num = random.randint(0, 10)  
+    numbers.append(num)
+
+    if num > 5:
+        greater_than_5 += 1
+
+    if num % 3 == 0:
+        divisible_by_3 += 1
+
+    counter += 1
+
+print(f"Numbers drawn: {numbers}")
+print(f"Numbers greater than 5: {greater_than_5}")
+print(f"Numbers divisible by 3: {divisible_by_3}")
