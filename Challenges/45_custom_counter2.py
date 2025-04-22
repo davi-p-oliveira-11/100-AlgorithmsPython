ddfb3bd6f446e88b547d4ff850d512c3dc379ca6
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 14:58:34 -0300

    updated algorithm 45 logic

--- Challenges/45_custom_counter2.py
+++ Challenges/45_custom_counter2.py
@@ -7,5 +7,20 @@ first_value = int(input("Enter the first value: "))
 last_value = int(input("Enter the last value: "))
 
 # Makes sure that the value is positive  
-
 increment = abs(int(input("Type the value of the increment: ")))
+
+counter = first_value
+
+if first_value <= last_value:
+  while counter <= last_value:
+    print(counter)
+    counter += increment
+  else:
+    while counter >= last_value:
+      print(counter)
+      counter -= increment
+
+
+print("It's over !")
+
+input("Press Enter to exit ... ")
