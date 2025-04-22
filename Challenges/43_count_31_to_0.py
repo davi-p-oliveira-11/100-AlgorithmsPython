5d00d86c140d01734605c431ad4c00a5952c48a8
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-22 14:08:06 -0300

    updated algorithm 43 python

--- Challenges/43_count_31_to_0.py
+++ Challenges/43_count_31_to_0.py
@@ -3,3 +3,17 @@
       that are divisible by 4, exactly as shown below:
       30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16]...
 '''
+
+counter = 31
+
+while counter >= 2:
+  counter -= 1
+  if counter % 4 == 0:
+    print(f"[{counter}]")
+  else:
+    print(counter)
+
+
+print("It's over !")
+
+input("Press Enter to exit ..." )
