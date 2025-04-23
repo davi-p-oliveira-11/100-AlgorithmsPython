bb0034d3f622759170ef60e20e9fe68db6a14e3d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 18:14:55 -0300

    added statement for algorithm 54

---
+++ Challenges/54_analyze_weight_height.py
@@ -0,0 +1,9 @@
+'''
+54) Develop a program that reads the weight and height of 7 people, displaying at the end:
+
+a) What was the average height of the group
+b) How many people weigh more than 90 kg
+c) How many people who weigh less than 50 kg are shorter than 1.60 m
+d) How many people who are taller than 1.90 m weigh more than 100 kg
+'''
+
