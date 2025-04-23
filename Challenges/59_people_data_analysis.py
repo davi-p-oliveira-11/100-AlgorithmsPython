27bf7c4ffb87a6c6b7ee18d8c7365fc6ec2448a5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 19:06:17 -0300

    updated algorithm 59 statement

---
+++ Challenges/59_people_data_analysis.py
@@ -0,0 +1,9 @@
+'''
+  59) Create a program that reads the gender and age of several people. 
+ The program will ask the user if they want to continue after each person. 
+ At the end, display:
+  a) The highest age recorded
+  b) The number of men registered
+  c) The age of the youngest woman
+  d) The average age among men
+'''
