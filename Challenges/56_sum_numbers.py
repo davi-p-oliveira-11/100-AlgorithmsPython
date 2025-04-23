1cb54284ea0d1da8ccb992d1ce2ae6f638526734
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 18:30:31 -0300

    updated algorithm 56 logic

--- Challenges/56_sum_numbers.py
+++ Challenges/56_sum_numbers.py
@@ -2,3 +2,18 @@
   56) Create a program that reads several numbers from the keyboard and shows the sum of them at the end.
      Note: The program will stop when the number 1111 is entered.
 '''
+
+sum_ = 0
+
+while True:
+    number = int(input("Enter a number (1111 to stop): "))
+
+    if number == 1111:
+        break
+
+    sum_ += number
+
+print(f"The sum of all entered numbers is: {sum_}")
+
+
+input("Press Enter to exit ... ")
