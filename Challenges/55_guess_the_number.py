af6718e02b86c72034b756cfdce7d45ba1f1649d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 18:18:23 -0300

    updated algorithm 55 statement

---
+++ Challenges/55_guess_the_number.py
@@ -0,0 +1,5 @@
+'''
+  55) [CHALLENGE] Let's improve the game we created in exercise 32. 
+  From now on, the computer will randomly select a number between 1 and 10, 
+  and the player will have 4 attempts to guess it.
+'''
