d23cce0a5965675f0306c54b05bdb7047ddb74e6
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 18:37:11 -0300

    updated algorithm 57 statement

---
+++ Challenges/57_employee_salary_analysis.py
@@ -0,0 +1,5 @@
+'''
+  57) Develop an application that reads the salary and gender of several employees.
+     At the end, display the total salary paid to men and the total paid to women.
+     The program will ask the user if they want to continue each time it reads the data of an employee.
+'''
