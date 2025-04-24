77ee05a587690c4ebc2e832e24090a365c71cc9e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:07:11 -0300

    updated algorithm 88

---
+++ Challenges/88_repeated_message_generator.py
@@ -0,0 +1,5 @@
+'''
+   88) Create a program that improves the Generator() procedure from the previous question 
+      so that it displays a message multiple times.
+      Example: When calling Generator("Learning Python", 4), it should display:
+'''
