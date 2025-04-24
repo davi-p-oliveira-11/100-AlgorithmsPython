1af3d5ec4ff45e2d27c8f29dadbf38b957522877
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:40:23 -0300

    updated algorithm 69 statement

---
+++ Challenges/69_arithmetic_progressions.py
@@ -0,0 +1,5 @@
+'''
+   69) [CHALLENGE] Develop a program that reads the first term and the common difference of an 
+      arithmetic progression (AP), displaying the first 10 elements of the AP and the sum of all 
+      values in the sequence.
+'''
