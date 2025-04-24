218390df79905feb2f117e016bc153f809132366
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:36:30 -0300

    updated algorithm 93 logic

--- Challenges/93_count_with_steps.py
+++ Challenges/93_count_with_steps.py
@@ -7,3 +7,24 @@
    For the input values: start (4), end (20), and step (3), calling Counter(4, 20, 3) will display:
    4 >> 7 >> 10 >> 13 >> 16 >> 19 >> END
 '''
+
+def counter(start: int, end: int, step: int) -> None:
+    for i in range(start, end + 1, step):
+        if i + step <= end:
+            print(f"{i} >>", end=' ')
+        else:
+            print(f"{i} >> END")
+
+try:
+    start = int(input("Enter the start value: "))
+    end = int(input("Enter the end value: "))
+    step = int(input("Enter the step value: "))
+
+    counter(start, end, step)
+except ValueError:
+    print("Invalid input. Please enter valid numbers for start, end, and step.")
+
+
+counter()
+
+input("Press Enter to exit ... ")
