b56269b4663743ef66fb1aa9e450ac3f5ddadb11
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 19:41:11 -0300

    updated algorithm 82 statement

---
+++ Challenges/82_class_grades.py
@@ -0,0 +1,7 @@
+'''
+  82) Create an algorithm that reads the grades of 10 students in a class and stores them in an array. At the end, display:
+     a) The class average
+     b) How many students scored above the class average
+     c) The highest grade entered
+     d) The positions where the highest grade appears
+'''
