6658931f18a6630584c87088cd67ffa3b593620a
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 17:16:48 -0300

    updated algorithm 77 logic

--- Challenges/77_reversal_names.py
+++ Challenges/77_reversal_names.py
@@ -3,3 +3,29 @@
       At the end, display a list with all the names in reverse order from the
       one they were entered.
 '''
+
+names = []
+
+for i in range(7):
+    name = input(f"Enter the name of person {i + 1}:").strip()
+    if name:
+        names.append(name)
+    else:
+        print("Invalid input. Please enter a valid name.")
+        i -= 1  
+
+
+i = 0
+while i < 7:
+    name = input(f"Enter the name of person {i + 1}:").strip()
+    if name:
+        names.append(name)
+        i += 1
+    else:
+        print("Invalid input. Please enter a valid name.")
+
+
+print("Names in reverse order:", ', '.join(reversed(names)))
+
+
+input("Press Enter to exit ... ")
