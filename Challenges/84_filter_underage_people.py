e8aa010607b04c33eb7122f1e40b3aeceda6eae1
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 19:52:40 -0300

    updated algorithm 84 statement

---
+++ Challenges/84_filter_underage_people.py
@@ -0,0 +1,5 @@
+'''
+  84) Create a program that reads the name and age of 9 people and stores these values in two arrays, 
+ keeping them in corresponding positions. 
+ In the end, display a list containing only the data of people who are underage.
+'''
