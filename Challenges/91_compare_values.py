7067ed5e2f044fac8616340fe6c83f02e6e94820
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:28:37 -0300

    updated algorithm 91 statement

---
+++ Challenges/91_compare_values.py
@@ -0,0 +1,5 @@
+'''
+  91) Develop an algorithm that reads two values from the keyboard and passes them to a procedure 
+  called Greatest() which will check which one is greater and display it on the screen. 
+  If both values are equal, display a message informing this characteristic.
+'''
