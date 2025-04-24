de01b62f3bbf2771d916d72a862f2c1e3b14c89c
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:57:10 -0300

    updated algorithm 96 logic

--- Challenges/96_student_name.py
+++ Challenges/96_student_name.py
@@ -2,3 +2,18 @@
   96) Create a program that has a function Average(), which will receive two grades 
       from a student and return their average to the main program.
 '''
+
+def average(grade1: float, grade2: float) -> float:
+    return (grade1 + grade2) / 2
+
+try:
+    input1 = float(input("Enter the first grade: "))
+    input2 = float(input("Enter the second grade: "))
+    
+    avg = average(input1, input2)
+    print(f"The average of the grades is: {avg}")
+except ValueError:
+    print("Invalid input. Please enter valid grades.")
+
+
+input("Press Enter to exit ... ")
