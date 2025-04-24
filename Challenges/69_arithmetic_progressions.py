c3172f695c25fe25e0a4ed9cf3db7afe96d9ed4e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:57:19 -0300

    updated algorithm 69

--- Challenges/69_arithmetic_progressions.py
+++ Challenges/69_arithmetic_progressions.py
@@ -3,3 +3,20 @@
       arithmetic progression (AP), displaying the first 10 elements of the AP and the sum of all 
       values in the sequence.
 '''
+
+first_term = float(input('Enter the first term of the AP: ') or '0')
+common_difference = float(input('Enter the common difference: ') or '0')
+
+ap_sequence = []
+sum_ap = 0
+
+for i in range(10):
+    term = first_term + i * common_difference
+    ap_sequence.append(term)
+    sum_ap += term
+
+print(f'First 10 elements of the AP: {", ".join(map(str, ap_sequence))}')
+print(f'Sum of all values in the sequence: {sum_ap}')
+
+
+input("Press Enter to Exit ... ")
