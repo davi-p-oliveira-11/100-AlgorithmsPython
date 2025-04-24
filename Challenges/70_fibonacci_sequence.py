74c6cefee9c8c200791b36d2c7936ab823948d3b
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 23:00:46 -0300

    updated algorithm 70

--- Challenges/70_fibonacci_sequence.py
+++ Challenges/70_fibonacci_sequence.py
@@ -3,3 +3,11 @@
      1 1 2 3 5 8 13 21...
 '''
 
+fibonacci = [1, 1]
+
+for i in range(2, 10):
+    fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+
+print(f'First 10 elements of the Fibonacci Sequence: {", ".join(map(str, fibonacci))}')
+
+input("Press Enter to Exit ... ")
