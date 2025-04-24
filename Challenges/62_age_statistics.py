52e44e93f4708a2fa6cdaa600e4efd4fd997d37d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 21:47:41 -0300

    updated algorithm 62 statement

---
+++ Challenges/62_age_statistics.py
@@ -0,0 +1,8 @@
+'''
+  62) Create a program using the "do while" loop that reads the age of multiple people. 
+  In each loop iteration, ask the user if they want to continue entering data.
+At the end, when the user decides to stop, display:
+a) The total number of ages entered
+b) The average of the entered ages
+c) How many people are 21 years old or older
+'''
