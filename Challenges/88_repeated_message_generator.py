6a4ad902e83de1aa4c62ef462ef161f64eeb6320
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:08:59 -0300

    updated algorithm 88

--- Challenges/88_repeated_message_generator.py
+++ Challenges/88_repeated_message_generator.py
@@ -3,3 +3,15 @@
       so that it displays a message multiple times.
       Example: When calling Generator("Learning Python", 4), it should display:
 '''
+
+def generator(message: str, repetitions: int) -> None:
+    line = '+-------=======------+'
+    for _ in range(repetitions):
+        print(line)
+        print(f'  {message}')
+        print(line)
+
+generator('Learning TypeScript', 4)
+
+
+input("Press Enter to exit ... ")
