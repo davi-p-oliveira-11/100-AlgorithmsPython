3d318d689daa2dcfbfcbb95ee157781424373d3e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 18:25:02 -0300

    updated algorithm 81 statement

---
+++ Challenges/81_age_analysis.py
@@ -0,0 +1,7 @@
+'''
+  81) Create a program that reads the ages of 8 people and stores them in an array. At the end, display:
+     a) The average age of the registered people
+     b) The positions where people older than 25 are found
+     c) The highest age entered (there may be repetitions)
+     d) The positions where the highest age was entered
+'''
