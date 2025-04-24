c23b18212e6a1e36e7ff0d6f563568c3fdcda686
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:30:12 -0300

    updated algorithm 91 logic

--- Challenges/91_compare_values.py
+++ Challenges/91_compare_values.py
@@ -3,3 +3,24 @@
   called Greatest() which will check which one is greater and display it on the screen. 
   If both values are equal, display a message informing this characteristic.
 '''
+
+def greatest(value1: float, value2: float) -> None:
+    if value1 > value2:
+        print(f"{value1} is greater than {value2}")
+    elif value2 > value1:
+        print(f"{value2} is greater than {value1}")
+    else:
+        print("Both values are equal.")
+
+try:
+    input1 = float(input("Enter the first value: "))
+    input2 = float(input("Enter the second value: "))
+    greatest(input1, input2)
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+
+
+greatest()
+
+
+input("Press Enter to exit ... ")
