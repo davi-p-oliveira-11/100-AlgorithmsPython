a8d17f86b7ea2a37cdcd201f32923a6e123de7e9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 17:13:13 -0300

    updated algorithm 77 statement

---
+++ Challenges/77_reversal_names.py
@@ -0,0 +1,5 @@
+'''
+  77) Create a program that reads 7 people's names and stores them in an array. 
+      At the end, display a list with all the names in reverse order from the
+      one they were entered.
+'''
