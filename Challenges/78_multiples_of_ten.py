ab15b35efd73749de4b2ba1ba179a11487c78e3e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 17:18:14 -0300

    updated algorithm 78

---
+++ Challenges/78_multiples_of_ten.py
@@ -0,0 +1,5 @@
+'''
+  78) Write a program that reads 15 numbers and stores them in an array. 
+    At the end, display the entire array on the screen and then show the 
+    positions where values that are multiples of 10 were entered.
+'''
