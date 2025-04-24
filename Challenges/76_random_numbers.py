e90e4adebc4096664cb04489183dab1c8a0e3fcf
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 17:03:35 -0300

    updated algorithm 76

--- Challenges/76_random_numbers.py
+++ Challenges/76_random_numbers.py
@@ -3,3 +3,12 @@
       numbers by the computer and then displays the generated values on the screen.
 '''
 
+import random
+
+arr6 = [random.randint(0, 9) for _ in range(7)]
+
+print(' '.join(map(str, arr6)))
+
+print(arr6)
+
+input("Press Enter to exit ... ")
