6e3a0fa768f99a14d796f904ca77d04156979b29
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:59:37 -0300

    updated algorithm 97 logic

--- Challenges/97_find-largest.py
+++ Challenges/97_find-largest.py
@@ -3,3 +3,17 @@
   Modify it to receive three numbers as parameters and return the largest among them.
 '''
 
+
+def greatest(num1: float, num2: float, num3: float) -> float:
+    return max(num1, num2, num3)
+
+try:
+    input1 = float(input("Enter the first number: "))
+    input2 = float(input("Enter the second number: "))
+    input3 = float(input("Enter the third number: "))
+
+    largest = greatest(input1, input2, input3)
+    print(f"The largest number is: {largest}")
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+
