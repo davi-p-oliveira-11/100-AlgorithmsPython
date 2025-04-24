ca390d472d0be12e9224796c1248e1c9cf896d31
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:45:29 -0300

    updated algorithm 95 logic

--- Challenges/95_summation_function.py
+++ Challenges/95_summation_function.py
@@ -3,3 +3,22 @@
   and return the sum of these values to the main program.
 '''
 
+def adder(value1: float, value2: float) -> float:
+    return value1 + value2
+
+try:
+    input6 = input("Enter the first value: ")
+    input7 = input("Enter the second value: ")
+
+    num5 = float(input6)
+    num6 = float(input7)
+
+    result = adder(num5, num6)
+    print(f"The sum of {num5} and {num6} is: {result}")
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+
+
+adder()
+
+input("Press Enter to exit ... ")
