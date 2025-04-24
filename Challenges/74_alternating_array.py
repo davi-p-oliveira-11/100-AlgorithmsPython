927dd22fa65afccb75620c0d5071fffa49d8048c
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 16:51:35 -0300

    updated algorithm 74

--- Challenges/74_alternating_array.py
+++ Challenges/74_alternating_array.py
@@ -5,3 +5,12 @@
    `0 1 2 3 4 5 6 7 8 9`  
 '''
 
+arr4 = [5 if i % 2 == 0 else 3 for i in range(10)]
+
+print(arr4)
+
+print(' '.join(map(str, arr4)))
+
+print(' '.join(map(str, range(10))))
+
+input("Press Enter to exit ... ")
