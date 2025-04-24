93925e79c2423b2ea059c37bcc82bff871a27309
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:39:42 -0300

    updated algorithm 94 statement

---
+++ Challenges/94_fibonacci_sequence.py
@@ -0,0 +1,12 @@
+'''
+   94) [CHALLENGE] Develop an application with a procedure called Fibonacci() that takes a single 
+  integer parameter, indicating how many terms of the sequence should be displayed on the screen. 
+  The procedure should receive this value and display the requested number of elements.
+
+  Note: Use exercises 70 and 75 to help with the solution.
+
+  Example:
+
+  Fibonacci(5) will generate: 1 >> 1 >> 2 >> 3 >> 5 >> END
+  Fibonacci(9) will generate: 1 >> 1 >> 2 >> 3 >> 5 >> 8 >> 13 >> 21 >> 34 >> END
+'''
