1a4197b87624a8f9109af99f92bdcc3df617e40d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:31:45 -0300

    updated algorithm 92

---
+++ Challenges/92_check_even_or_odd.py
@@ -0,0 +1,4 @@
+'''
+   92) Create a logic that reads an integer and passes it to a procedure called EvenOrOdd() 
+      which will check and display on the screen whether the value passed as a parameter is EVEN or ODD.
+'''
