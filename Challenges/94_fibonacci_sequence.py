1b163017e46f58639a74504f65a8a4986aa8655e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:41:45 -0300

    updated algorithm 94 logic

--- Challenges/94_fibonacci_sequence.py
+++ Challenges/94_fibonacci_sequence.py
@@ -10,3 +10,27 @@
   Fibonacci(5) will generate: 1 >> 1 >> 2 >> 3 >> 5 >> END
   Fibonacci(9) will generate: 1 >> 1 >> 2 >> 3 >> 5 >> 8 >> 13 >> 21 >> 34 >> END
 '''
+
+def fibonacci(n: int) -> None:
+    a, b = 1, 1
+
+    for i in range(1, n + 1):
+        if i == n:
+            print(f"{a} >> END")
+        else:
+            print(f"{a} >>", end=' ')
+        a, b = b, a + b
+
+try:
+    num_terms = int(input("Enter the number of Fibonacci terms you want to display: "))
+    if num_terms > 0:
+        fibonacci(num_terms)
+    else:
+        print("Invalid input. Please enter a valid number.")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+
+
+fibonacci()
+
+input("Press Enter to exit ... ")
