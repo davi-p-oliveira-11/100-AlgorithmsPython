823bc6dfb46de1e78054ad93cd194b06494645c4
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:34:35 -0300

    updated algorithm 93

---
+++ Challenges/93_count_with_steps.py
@@ -0,0 +1,9 @@
+''' 
+  93) Create a program with a procedure called Counter() that takes three values as parameters:
+  the start, the end, and the step of a count. 
+  The main program should request these values from the user and pass them to the procedure,
+  which will then display the count on the screen.
+   Example:
+   For the input values: start (4), end (20), and step (3), calling Counter(4, 20, 3) will display:
+   4 >> 7 >> 10 >> 13 >> 16 >> 19 >> END
+'''
