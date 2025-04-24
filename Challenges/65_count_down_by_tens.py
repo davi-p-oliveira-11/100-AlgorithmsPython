6894818a7d8f65e8d278fd64de60a52befb5c906
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:08:40 -0300

    updated algorithm 65 logic

--- Challenges/65_count_down_by_tens.py
+++ Challenges/65_count_down_by_tens.py
@@ -2,3 +2,10 @@
   65) Develop a program using the "for" loop structure that displays the following count on the screen:
      100 90 80 70 60 50 40 30 20 10 0 Done!
 '''
+
+for i in range(100, -1, -10):
+    print(i)
+
+print("It's over !")
+
+input("Press Enter to exit ... ")
