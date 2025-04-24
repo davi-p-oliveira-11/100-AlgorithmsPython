a8c975b9dcc904c66f41ca5db241af7540c280a4
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:37:40 -0300

    updated algorithm 68 logic

--- Challenges/68_gender_weight_stats.py
+++ Challenges/68_gender_weight_stats.py
@@ -7,3 +7,40 @@
      d) The highest weight among the men
 '''
 
+class Person:
+    def __init__(self, gender, weight):
+        self.gender = gender
+        self.weight = weight
+
+people = []
+
+for i in range(8):
+    gender = input(f'Enter gender for person {i + 1} (M/F): ').upper()
+    weight = float(input(f'Enter weight for person {i + 1}: ') or '0')
+
+    people.append(Person(gender, weight))
+
+women_count = 0
+men_over_100kg = 0
+women_weight_sum = 0
+heaviest_man = 0
+
+for person in people:
+    if person.gender == 'F':
+        women_count += 1
+        women_weight_sum += person.weight
+    else:
+        if person.weight > 100:
+            men_over_100kg += 1
+        if person.weight > heaviest_man:
+            heaviest_man = person.weight
+
+women_average_weight = women_weight_sum / women_count if women_count > 0 else 0
+
+print(f'Number of women registered: {women_count}')
+print(f'Number of men weighing more than 100Kg: {men_over_100kg}')
+print(f'Average weight of the women: {women_average_weight:.2f} Kg')
+print(f'Highest weight among the men: {heaviest_man} Kg')
+
+
+input("Press Enter to exit ... ")
