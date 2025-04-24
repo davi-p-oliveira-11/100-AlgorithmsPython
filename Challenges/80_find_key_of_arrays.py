5816dbc1ca28c01e9c553d7d52d5442d00a3fd70
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 18:21:10 -0300

    updated algorithm 80 statement

---
+++ Challenges/80_find_key_of_arrays.py
@@ -0,0 +1,7 @@
+'''
+    80) Create an algorithm that fills a 30-position array with randomly generated numbers 
+        between 1 and 15. After that, ask the user to enter a number (key), and your program 
+        should display the positions where this key was found.
+        Also, show how many times the key was drawn.
+'''
+
