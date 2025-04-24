354fbbc4d54e6a95ff1b08826d2d2160415ef1bd
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:26:14 -0300

    updated algorithm 67 statetement

---
+++ Challenges/67_count_up_to.py
@@ -0,0 +1,7 @@
+'''
+  67) Create a program using the "for" loop that reads a positive integer and displays a count 
+     from 0 to the entered value.
+
+     Enter a number: 9  
+     Count: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, END !
+'''
