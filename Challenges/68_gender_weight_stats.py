81f5464fa0aaf7530b7340c4ac32fc5b7093db8a
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:35:56 -0300

    updated algorithm 68 statement

---
+++ Challenges/68_gender_weight_stats.py
@@ -0,0 +1,9 @@
+'''
+  68) Create a program that reads the gender and weight of 8 people using the "for" loop. 
+ At the end, display:
+     a) How many women were registered
+     b) How many men weigh more than 100Kg
+     c) The average weight of the women
+     d) The highest weight among the men
+'''
+
