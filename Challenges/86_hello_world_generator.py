a712463ab156987fc4af2f16ad2e8d0f0b738704
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 20:00:36 -0300

    updated algorithm 86

---
+++ Challenges/86_hello_world_generator.py
@@ -0,0 +1,10 @@
+'''
+  86) Create a program that has a procedure called Generator() that, when called, 
+     displays the message "Hello, World!" with some visual components (lines).
+     Example: When calling Generator(), it should display:
+
+     +-------=======------+
+       Hello, World !
+     +-------=======------+
+'''
+
