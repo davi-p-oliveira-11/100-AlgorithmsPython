4d2524d49a8d357e973233925103d5000d3440cb
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 21:52:31 -0300

    updated algorithm 63 statement

---
+++ Challenges/63_number_analysis.py
@@ -0,0 +1,10 @@
+'''
+  63) Create a program using the "do while" loop that reads multiple numbers. 
+ In each loop iteration, ask the user if they want to continue.
+At the end, display:
+a) The sum of all values
+b) The smallest number entered
+c) The average of all values
+d) How many values are even
+'''
+
