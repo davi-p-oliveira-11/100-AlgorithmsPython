32231cfe5738b6341e76374c6f006387e93e9d94
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 18:10:44 -0300

    updated algorithm 79

---
+++ Challenges/79_even_numbers_positions.py
@@ -0,0 +1,5 @@
+'''
+   79) Develop a program that reads 10 integers and stores them in an array. 
+       At the end, display which even numbers were entered and the positions 
+       where they are stored.
+'''
