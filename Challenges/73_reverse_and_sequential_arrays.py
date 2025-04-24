0de7437ee090d4e158482d63a5a537058e865058
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 16:48:39 -0300

    updated algorithm 73 statement

--- Challenges/73_reverse_and_sequential_arrays.py
+++ Challenges/73_reverse_and_sequential_arrays.py
@@ -3,3 +3,13 @@
      9 8 7 6 5 4 3 2 1 0
      0 1 2 3 4 5 6 7 8 9
 '''
+
+arr3 = [i for i in range(9, -1, -1)]
+
+print(arr3)  
+
+print(' '.join(map(str, arr3)))
+
+print(' '.join(map(str, range(10))))
+
+input("Press Enter to exit ...")
