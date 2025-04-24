95b96f4fa39953adc282b2c09557fb1fd0fe08c2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 16:50:01 -0300

    updated algorithm 74 statement

---
+++ Challenges/74_alternating_array.py
@@ -0,0 +1,7 @@
+'''
+ 74) Create a program that automatically fills (using logic, not just direct assignment) 
+    a numerical array with 10 positions, as follows:  
+   `5 3 5 3 5 3 5 3 5 3`  
+   `0 1 2 3 4 5 6 7 8 9`  
+'''
+
