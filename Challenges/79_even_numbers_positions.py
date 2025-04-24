bb80649d7d6dc6460162554b30338510c86790d9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 18:13:26 -0300

    updated algorithm 79 logic

--- Challenges/79_even_numbers_positions.py
+++ Challenges/79_even_numbers_positions.py
@@ -3,3 +3,27 @@
        At the end, display which even numbers were entered and the positions 
        where they are stored.
 '''
+
+numbers2 = []
+even_numbers = []
+even_indexes = []
+
+i = 0
+while i < 10:
+    try:
+        input_value = input(f"Enter integer {i + 1}: ")
+        num = int(input_value)
+        numbers2.append(num)
+        if num % 2 == 0:
+            even_numbers.append(num)
+            even_indexes.append(i)
+        i += 1
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+print("Numbers entered:", ', '.join(map(str, numbers2)))
+print("Even numbers:", ', '.join(map(str, even_numbers)))
+print("Positions of even numbers:", ', '.join(map(str, even_indexes)))
+
+
+input("Press Enter to exit ... ")
