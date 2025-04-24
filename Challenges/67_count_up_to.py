69e0bac9b8a0af1fe5b2393e9c8cf0209b39ef9d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-23 22:27:30 -0300

    updated algorithm 67

--- Challenges/67_count_up_to.py
+++ Challenges/67_count_up_to.py
@@ -5,3 +5,10 @@
      Enter a number: 9  
      Count: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, END !
 '''
+
+entered_number = int(input('Enter a positive number: '))
+
+for counter in range(0, entered_number + 1):
+    print(counter)
+
+print('END !')
