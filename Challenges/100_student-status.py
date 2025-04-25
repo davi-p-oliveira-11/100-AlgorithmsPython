483a5164a035546aa41b2f0b5544dd6180238c1b
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 21:09:30 -0300

    updated algorithm 100 statement

---
+++ Challenges/100_student-status.py
@@ -0,0 +1,6 @@
+'''
+ 100) Improve exercise 96 by creating, in addition to the Average() function, 
+  another function called Situation(), which will return to the main program 
+  whether the student is APPROVED, in RECOVERY, or FAILED. 
+  This new function will receive as a parameter the result returned by the Average() function.
+'''
