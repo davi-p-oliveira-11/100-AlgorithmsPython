ff649418e1b1ec47d34842ca373cc0233f5c107c
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 21:01:27 -0300

    updated algorithm 98 statement

---
+++ Challenges/98_range_sum.py
@@ -0,0 +1,8 @@
+'''
+   98) Create a program that has a function SuperAdder(), which will receive two numbers 
+  as parameters and return the sum of all values in the range between the received numbers.
+
+  Example:
+  SuperAdder(1, 6) will sum 1 + 2 + 3 + 4 + 5 + 6 and return 21
+  SuperAdder(15, 19) will sum 15 + 16 + 17 + 18 + 19 and return 85
+'''
