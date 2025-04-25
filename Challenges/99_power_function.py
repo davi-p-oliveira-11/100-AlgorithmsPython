d1a9d052d1865b14edb592e1ef8517ee58d28e41
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-04-24 21:05:00 -0300

    updated algorithm 99 statement

---
+++ Challenges/99_power_function.py
@@ -0,0 +1,8 @@
+'''
+  99) Create a program that has a function called `Power()`, 
+  which will receive two numerical parameters (base and exponent) 
+  and calculate the result of exponentiation.  
+
+  **Example:**  
+  `Power(5,2)` will calculate **5² = 25**  
+'''
